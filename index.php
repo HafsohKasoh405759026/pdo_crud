234ac3d89f93cb23259950ec96b84c9a2fa85205
HafsohKasoh405759026 <Lada.2538@gmail.com>
2017-05-22 16:40:17 +0700

    สร้างไฟล์ create.php

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
-<?php 
-phpinfo();
-
+<?php
+require_once("libs/Db.php");
+$objDb = new Db();
+$db = $objDb->database;
 ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>PDO & CRUD</title>
+  </head>
+  <body>
+
+    <?php
+    echo "Hello PDO";
+     ?>
+
+  </body>
+</html>
